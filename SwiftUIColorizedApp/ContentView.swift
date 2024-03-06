5bb3c41bec88a6e0926396bf0707188ec4c83a7f
Konstantin <diel404@gmail.com>
2024-03-05 23:59:53 -0800

    Create SwiftUI app

--- SwiftUIColorizedApp/ContentView.swift
+++ SwiftUIColorizedApp/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    @State private var displayedValue = 0
+    @State private var userRedSliderValue = ""
+    @State private var userGreenSliderValue = ""
+    @State private var userBlueSliderValue = ""
+
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 40) {
+            Image(systemName: "rectangle.fill")
+                .resizable()
+                .foregroundStyle(Color(red: redSliderValue / 255, green: greenSliderValue / 255, blue: blueSliderValue / 255))
+                .frame(width: 300, height: 200)
+            ColorSliderView(sliderValue: $redSliderValue, userSliderValue: $userRedSliderValue, colorSlider: Color.red)
+            ColorSliderView(sliderValue: $greenSliderValue, userSliderValue: $userGreenSliderValue, colorSlider: Color.green)
+            ColorSliderView(sliderValue: $blueSliderValue, userSliderValue: $userBlueSliderValue, colorSlider: Color.blue)
+
+            Spacer()
         }
         .padding()
     }
@@ -21,4 +36,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ColorSliderView: View {
+    @Binding var sliderValue: Double
+    @Binding var userSliderValue: String
+    var colorSlider: Color
+    
+    var body: some View {
+        HStack {
+            Text(lround(sliderValue).formatted())
+                .frame(width: 45)
+                .foregroundStyle(colorSlider)
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+
+            TextField("", text: $userSliderValue)
+                .frame(width: 45, height: 25)
+                .foregroundStyle(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(lineWidth: 2)
+                )
+                .keyboardType(.numberPad)
+            
+                
+        }
+    }
 }
