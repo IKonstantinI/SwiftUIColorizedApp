ace2bae3d2e368470b6e68802b8d6e0b63316fc4
Konstantin <diel404@gmail.com>
2024-03-05 21:16:12 -0800

    Initial Commit

---
+++ SwiftUIColorizedApp/SwiftUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedAppApp.swift
+//  SwiftUIColorizedApp
+//
+//  Created by horze on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
